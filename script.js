0fc2bb482637983d5b723ec7c21792014ca4ba9d
Hava Maria Maszetta Ruudjitto <89438819+Havamariamvfr@users.noreply.github.com>
2024-12-17 21:11:14 +0700

    update

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-    const map = L.map('map').setView([51.505, -0.09], 13); 
-    // Initialize map and set view to initial coordinates
+// Inisialisasi Map Utama
+const mainMap = L.map("main-map").setView([-6.9826, 110.4145], 12);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "&copy; OpenStreetMap contributors",
+}).addTo(mainMap);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map); 
-    // Add tile layer to the map
-
-    // Watch for user's geolocation
-    navigator.geolocation.watchPosition(success, error);
-
-    function success(pos) {
-        const lat = pos.coords.latitude;
-        const lng = pos.coords.longitude;
-        const accuracy = pos.coords.accuracy;
-
-        // Add marker for user position
-        L.marker([lat, lng]).addTo(map).bindPopup('You are here!').openPopup();
-        L.circle([lat, lng], { radius: accuracy }).addTo(map);
-        map.setView([lat, lng]); // Center map on user position
-    }
-
-    function error(err) {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
+// Fungsi untuk Fly ke Lokasi
+function flyToLocation(lng, lat) {
+  mainMap.flyTo([lat, lng], 15);
+}
